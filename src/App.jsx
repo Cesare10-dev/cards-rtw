1b011adc05bd383c5dbcab45ff1abdc803190efe
Cesare10-dev <cesare1087@gmail.com>
2023-01-25 23:22:45 +0100

    tailwind animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Card from "./Card";
+import img1 from "./img/img1.jpg";
+import img2 from "./img/img2.jpg";
+import img3 from "./img/img3.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="flex justify-center items-center h-screen gap-8 bg-gradient-to-r from-sky-300 to-sky-600">
+      <Card title="Joystick" description="PS4 joystick." image={img1} />
+      <Card title="Joystick" description="PS4 joystick." image={img2} />
+      <Card title="Joystick" description="PS4 joystick." image={img3} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+const Card = ({ title, description, image, colors }) => {
+  return (
+    <div className="h-[380px] w-60 rounded-lg shadow-xl cursor-pointer hover:-translate-y-8 transition active:rotate-180 duration-500">
+      <img src={image} className="h-[310px] w-full object-cover rounded-t-lg" />
+      <div className="h-[70px] rounded-b-lg p-2 bg-blue-500">
+        <h2 className="text-white text-2xl font-medium">{title}</h2>
+        <p className="font-medium text-xs">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
